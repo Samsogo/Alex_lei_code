3743e4575e5e6e68e3a8d0d12e48fae327f2e7df
Samsogo <1150693181@qq.com>
2018-11-25 15:18:03 +0800

    My Scala Project

---
+++ IdeaProjects/Scala/src/Scala_test.scala
@@ -0,0 +1,11 @@
+
+/**
+  * When we learn a language,we must start with Hello World
+  */
+
+
+object Scala_test {
+  def main(args: Array[String]): Unit = {
+    System.out.print("Hello World")
+  }
+}
