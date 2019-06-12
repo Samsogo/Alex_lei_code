1a18d6f65570058af8b7b4dfd51fbf313a0e0f5c
Samsogo <1150693181@qq.com>
2019-06-12 19:01:56 +0800

    新增数据流demo

--- AllCode/flink/src/main/scala/DataStream/DataStream_test.scala
+++ AllCode/flink/src/main/scala/DataStream/DataStream_test.scala
@@ -1,34 +1,40 @@
 package DataStream
 
-import java.util
 
+import org.apache.flink.api.common.accumulators.IntCounter
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.api.scala._
+//import org.apache.flink.api.scala._
 
 
 
 /**
   * @Author:Alex_lei
-  * @Description: Datastream
+  * @Description: Datastream_demo
   */
 object DataStream_test {
 
-   case class Order(val name:String, val age:Int, val product:String,val amount:Int)
-
   def main(args: Array[String]): Unit = {
-
+    /**
+      * 获取本地运行环境，本地测试通常使用getExecutionEnvironment
+      * 还可以通过StreamExecutionEnvironment.createLocalEnvironment()
+      * StreamExecutionEnvironment.createRemoteEnvironment(host: String, port: Int, jarFiles: String*)
+      */
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    val data = util.Arrays.asList(Order("Tim",12,"apple",20),
-      Order("Tim",12,"water",2),
-      Order("Lily",13,"milk",5),
-      Order("Bob",14,"beer",6))
+    /**
+      * 在此举例说明读取外部文件作为数据源，后续还会说明通过不同的方式获取数据源
+      */
+    val data = env.readTextFile("file:///Users/Alex_Lei/Desktop/a.txt")
 
-    val datastream = env.fromElements(data)
+    /**
+      * 对数据源进行相关的操作，类似于Spark的RDD算子操作，后续也会举例说明
+      */
+    data.print("datastream")
 
-    val d = datastream.map(x=>(x.get(0)))
-
-    d.print()
+    /**
+      * 最后必须加这一行代码，才会触发执行程序。
+      * 因为flink的程序都是懒加载模式，即上述所写的读取外部文件和对数据流的一系列操作只有调用execute方法才会执行
+      */
 
     env.execute("test")
   }
